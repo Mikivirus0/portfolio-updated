c99458d7dafcc9201c538b49f7f7e4b1097ebc2c
mikivirus0 <mumairs3636@gmail.com>
2025-06-10 22:41:18 +0500

    Updated Main Page

--- app/config.ts
+++ app/config.ts
@@ -1,10 +1,10 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://portfolio-updated-git-main-mikivirus-projects.vercel.app/",
+  title: "MikiVirus",
+  name: "MikiVirus",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "Theme From nextfolio-template.vercel.app",
 };
 
 export const socialLinks = {
