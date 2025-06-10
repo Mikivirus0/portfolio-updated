c51e5b624341424f30f1cf2e44bf48829d827cbb
mikivirus0 <mumairs3636@gmail.com>
2025-06-10 21:17:59 +0500

    Added Intro

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,20 @@ export default function Page() {
         />
       </a>
       <h1 className="mb-8 text-2xl font-medium">
-        Umair — Cybersecurity Analyst & Red Team Operator
+        Umair — Cybersecurity Analyst
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
           I’m a cybersecurity professional with expertise in Red Teaming, Penetration Testing, and Digital Forensics. With a strong background in adversary simulation and vulnerability assessments, I specialize in identifying real-world threats and helping organizations strengthen their security posture.
         </p>
         <p>
+          I’m currently pursuing my MS in Cyber Security from <strong>Air University</strong>, where I’m further enhancing my technical depth and research capability in advanced topics like threat intelligence, malware analysis, and cloud security.
+        </p>
+        <p>
           I’ve led the development of CTF challenges and infrastructure at AirOverflow, organized international cybersecurity competitions, and contributed to open-source tooling and secure development projects.
         </p>
         <p>
-          Some of my key achievements include winning the Hackmasters’24 (OIC CTF – Istanbul), becoming a finalist at BlackHat MEA’23 & ’24 (Riyadh), and leading national-level cybersecurity trainings at Bahria University under Ignite.
+          Some of my key achievements include winning the Hackmasters’24 (OIC CTF – Istanbul), becoming a finalist at BlackHat MEA’23 & ’24 (Riyadh), winning 20+ national level hackathons and leading national-level cybersecurity trainings at Bahria University under Ignite.
         </p>
         <p>
           Certified in CRTO, and eCPPT. Active on platforms like Hack The Box and TryHackMe, where I regularly sharpen my skills through real-world simulation labs.
