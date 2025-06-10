7eb2272f031541b057a47d1c410db4e22953c19a
mikivirus0 <mumairs3636@gmail.com>
2025-06-10 20:33:45 +0500

    Added Projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,15 +7,21 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Mithril AI",
-    year: 2024,
-    description: "Open science AI resarch lab",
-    url: "https://github.com/mithrilai",
+    title: "Sqlhound",
+    year: 2025,
+    description: "A lightweight multithreaded SQL Injection (SQLi) vulnerability scanner using Bing dorking, subdomain enumeration, and custom URL lists.",
+    url: "https://github.com/Mikivirus0/sqlhound",
   },
   {
-    title: "OpenDeepLearning",
+    title: "Subroot",
+    year: 2025,
+    description: "Subroot is a high-performance subdomain scanner designed for security researchers and penetration testers. It leverages DNS resolution, HTTP requests, and ICMP pings to identify active subdomains efficiently.",
+    url: "https://github.com/Mikivirus0/SUBROOT",
+  },
+  {
+    title: "Gradient Obfuscation",
     year: 2023,
-    description: "Open source AI education resources",
-    url: "https://opendeeplearning.xyz/",
+    description: "A Python program that combines three image processing techniques: denoising, enhancement, and style transfer, to obfuscate an input image.",
+    url: "https://github.com/Mikivirus0/gradient-obfuscation",
   },
 ];
